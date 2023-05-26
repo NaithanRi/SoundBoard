5af6384c831313429d1cca02e13e0b78a3af0854
201139837 <naithanr2005@gmail.com>
2023-05-26 13:51:22 -0700

    updates!!

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -15,6 +15,26 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnarcadeGameover;
 
+    private Button btnalientalk;
+
+    private Button btnfalling;
+
+    private Button btnscifibleep;
+
+    private Button btnsneeze;
+
+    private Button btnhorrorchaos;
+
+    private Button btnfalling;
+
+    private Button falling;
+
+    private Button falling;
+
+    private Button falling;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
         btnBleep = findViewById(R.id.btn_bleep);
         btnWoops = findViewById(R.id.btn_woops);
         btnarcadeGameover = findViewById(R.id.btn_gameover);
+        btnalientalk = findViewById(R.id.btn_alientalk);
+        btnalientalk = findViewById(R.id.btn_falling);
+        btnalientalk = findViewById(R.id.btn_scifibleep);
+        btnalientalk = findViewById(R.id.btn_sneeze);
+        btnalientalk = findViewById(R.id.btn_horrorchaos);
+
+
 
         btnBleep.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +72,36 @@ public class MainActivity extends AppCompatActivity {
                 playArcade();
             }
         });
-
+        btnalientalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAlientalk();
+            }
+        });
+        btnfalling.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playFalling();
+            }
+        });
+        btnscifibleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playScifibleep();
+            }
+        });
+        btnsneeze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSneeze();
+            }
+        });
+        btnhorrorchaos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playHorrorchaos();
+            }
+        });
 
     }
 
@@ -75,6 +131,63 @@ public class MainActivity extends AppCompatActivity {
 
     private void playArcade(){
         MediaPlayer mp = MediaPlayer.create(this,R.raw.sound_arcadegameover);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+                mp=null;
+            }
+        });
+    }
+
+    private void playAlientalk(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sound_alientalk);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+                mp=null;
+            }
+        });
+    }
+
+    private void playFalling(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sound_falling);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+                mp=null;
+            }
+        });
+    }
+    private void playScifibleep(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sound_scifibleep);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+                mp=null;
+            }
+        });
+    }
+    private void playSneeze(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sound_sneeze);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+                mp=null;
+            }
+        });
+    }
+    private void playHorrorchaos(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sound_horrorchaos);
         mp.start();
         mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
